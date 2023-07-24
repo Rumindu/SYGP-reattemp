5809b82d089ed05c4d3cb745af4d81ac514cb9a6
Rumindu De Silva <rumindukavishka14@gmail.com>
2023-07-24 15:22:25 +0530

    create view and add announcement functionality

--- app/controllers/Announcements.php
+++ app/controllers/Announcements.php
@@ -6,12 +6,55 @@
 
     public function index(){
       $announcements = $this->AnnouncementModel->getAnnouncements();
-
       $data = [
-        'title' => 'Welcome123',
-        'announcements' => $announcements
+        'announcements' => $announcements,
       ];
-      $this->view('pages/index', $data);
+      $this->view('announcements/v_index', $data);
+    }
+
+    public function add(){
+      if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+        
+        $data = [
+          'title' => trim($_POST['title']),
+          'content' => trim($_POST['content']),
+          'title_err' => '',
+          'content_err' => ''
+        ];
+
+        if(empty($data['title'])){
+          $data['title_err'] = 'Please enter a title.';
+        }
+        if(empty($data['content'])){
+          $data['content_err'] = 'Please enter content.';
+        }
+
+        if(empty($data['title_err']) && empty($data['content_err'])){
+          if($this->AnnouncementModel->addAnnouncement($data)){
+            //print_r($data);
+            redirect('announcements/index');
+          }
+          else {
+            die('Something went wrong.');
+          }
+        
+        }
+        else{
+        $this->view('announcements/v_add', $data);
+        }
+      }
+
+      //loading default view. At initially (/Announcements/add) load this empty view
+      else {
+        $data = [
+          'title' => '',
+          'content' => '',
+          'title_err' => '',
+          'content_err' => ''
+        ];
+        $this->view('announcements/v_add', $data);
+      }
     }
 
     
