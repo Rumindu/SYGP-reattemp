54d6cda0346efb0524da38893a9229983bcb26f7
Rumindu De Silva <rumindukavishka14@gmail.com>
2023-08-06 22:48:43 +0530

    Replace input area by text area and complete all styles

--- app/views/announcements/v_add.php
+++ app/views/announcements/v_add.php
@@ -2,18 +2,25 @@
 <?php require APPROOT. '/views/inc/topnavbar.php'; ?>
 <?php require APPROOT. '/views/inc/sidebar.php'; ?>
   
-    <h1>Create an Announcement</h1>
+<div class="body-container" style="height: 100vh;">    
+  <h1>Create an Announcement</h1>
     <div class="announcement-container">
       <form action="" method="post">
-        <input type="text" name="title"
-        placeholder="Title" value="<?php echo $data['title'];?>">
-        <span class="form-invalid"><?php echo $data['title_err'];?></span>
-        <input type="text" name="content" placeholder="Content" value="<?php echo $data['content'];?>">
-        <span class="form-invalid"><?php echo $data['content_err'];?></span>
+      <label for="title"><h2 style="margin:0px">Title</h3></label>
+        <input id="title" type="text" name="title" placeholder="Enter the announcement title" value="<?php echo $data['title'];?>">
+        <span class="form-invalid"><?php echo $data['title_err'];?>
+        </span>
+        <br>
+        <label for="content"><h2 style="margin:0px">Content</h3></label>
+        <textarea name="content" id="content" placeholder="Enter the announcement content" cols="30" rows="10" class="form-text-area"></textarea>
+        <span class="form-invalid"><?php echo $data['content_err'];?>
+        </span>
         <br><br>
-        <input type="submit" value="Submit">
-        
+        <input class="submit-btn" type="submit" value="Submit">
+        <button type="button" class="delete-btn" onclick="window.location.href='<?php echo URLROOT?>/Announcements/index'">Cancel
+        </button>
       </form>
     </div>
+</div>
   
 <?php require APPROOT. '/views/inc/footer.php'; ?>
--- app/views/announcements/v_edit.php
+++ app/views/announcements/v_edit.php
@@ -2,18 +2,28 @@
 <?php require APPROOT. '/views/inc/topnavbar.php'; ?>
 <?php require APPROOT. '/views/inc/sidebar.php'; ?>
   
-    <h1>Create an Announcement</h1>
+  <div class="body-container" style="height: 100vh;"> 
     <div class="announcement-container">
+      <h1 style="text-align:center">Edit an Announcement</h1>
       <form action="" method="post">
-        <input type="text" name="title"
+        <label for="title"><h2 style="margin:0px">Title</h3></label>
+        <input type="text" name="title" id="title"
         placeholder="Title" value="<?php echo $data['title'];?>">
         <span class="form-invalid"><?php echo $data['title_err'];?></span>
-        <input type="text" name="content" placeholder="Content" value="<?php echo $data['content'];?>">
+        <br>
+        <label for="content"><h2 style="margin:0px">Content</h3></label>
+        <textarea name="content" id="content" cols="30" rows="10" class="form-text-area">
+          <?php echo $data['content'];?> 
+        </textarea>
         <span class="form-invalid"><?php echo $data['content_err'];?></span>
         <br><br>
-        <input type="submit" value="Submit">
-        
+        <div style="display: flex; justify-content: center; gap: 20px;">
+          <input class="submit-btn" type="submit" value="Submit">
+          <?php require APPROOT. '/views/inc/footer.php'; ?>
+          <!-- <a href="<?//php echo URLROOT?>/announcements/index"><button>Cancel</button></a> //this one is redirecting to the Index page. But db query is executing -->
+          <button type="button" class="delete-btn" onclick="window.location.href='<?php echo URLROOT?>/Announcements/index'" ">Cancel</button>
+        </div>
       </form>
     </div>
+  </div>
   
-<?php require APPROOT. '/views/inc/footer.php'; ?>
