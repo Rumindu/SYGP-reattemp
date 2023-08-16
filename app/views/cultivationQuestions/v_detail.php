c0a4cac85cad3b70968941e3b049b3ba88b6d00c
M. R. K. W. DE SILVA <108813657+Rumindu@users.noreply.github.com>
2023-08-16 18:47:50 +0530

    add cultivation response edit delete button

--- app/views/cultivationQuestions/v_detail.php
+++ app/views/cultivationQuestions/v_detail.php
@@ -19,15 +19,30 @@
     <br><br>
     <?php break;?>
   <?php endforeach; ?>
-  <a href="<?php echo URLROOT;?>/CultivationQuestionsResponse/add/<?php echo $cultivationQuestion->question_id;?>"><button class="publish-announcement-btn"><h2>Publish announcement</h2></button></a>
+  <a href="<?php echo URLROOT;?>/CultivationQuestionsResponse/add/<?php echo $cultivationQuestion->question_id;?>"><button class="publish-announcement-btn"><h2>Add a response</h2></button></a>
   </div>
 
   <?php foreach($data['CultivationQuestion'] as $cultivationQuestion) : ?>
   <div class="announcement-container">
     <p><?php echo $cultivationQuestion->content;?></p>
+    <?php
+        if($_SESSION['user_id'] == $cultivationQuestion->agri_officer_id){?>
+          <div style="Display:flex; gap:5px">
+            <a href="<?php echo URLROOT ."/CultivationQuestionsResponse/edit/".$cultivationQuestion->response_id?>">
+              <button class="edit-btn">
+                Edit
+              </button>
+            </a>
+            <a href="<?php echo URLROOT ."/CultivationQuestionsResponse/delete/".$cultivationQuestion->response_id?>">
+              <button class="delete-btn">
+                Delete
+              </button>
+            </a>
+          </div>
+        <?php }?>
         <div style="display:flex; justify-content: flex-start;">
-          <h3><?php echo $cultivationQuestion->producer_name;?> - </h3>
-          <h3><?php echo $cultivationQuestion->asked_date_time;?></h3>
+          <h3><?php echo $cultivationQuestion->agri_officer_name;?> - </h3>
+          <h3><?php echo $cultivationQuestion->responded_date_time;?></h3>
         </div>
         <div class="announcement-container">
     </div>
