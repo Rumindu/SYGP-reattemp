ae4d05510fe3f6b6270f63ddf20da23fb8bbf552
M. R. K. W. DE SILVA <108813657+Rumindu@users.noreply.github.com>
2023-08-17 11:14:03 +0530

    Complete stage 1 of SYGP

--- app/views/inc/sidebar.php
+++ app/views/inc/sidebar.php
@@ -2,7 +2,5 @@
 <img src="<?php echo URLROOT."/public/img/navbar-logo-large.webp";?>" alt="logo" height="72px">
   <a href="<?php echo URLROOT; ?>/Announcements/Index">Announcements</a>
   <a href="<?php echo URLROOT; ?>/CultivationDetails/Index">Producer Details</a>
-  <a href="">Land Details</a>
-  <a href="">Cultivation Details</a>
   <a href="<?php echo URLROOT; ?>/CultivationQuestions/Index">Cultivation question</a>
 </div>
