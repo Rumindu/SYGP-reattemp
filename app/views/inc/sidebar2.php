cd2415bd99d206e4ab409978a22e11bdb1c2f446
M. R. K. W. DE SILVA <108813657+Rumindu@users.noreply.github.com>
2023-08-23 17:03:00 +0530

    Create sidebar for producer

---
+++ app/views/inc/sidebar2.php
@@ -0,0 +1,5 @@
+<div class="sidebar">
+<img src="<?php echo URLROOT."/public/img/navbar-logo-large.webp";?>" alt="logo" height="72px">
+  <a href="<?php echo URLROOT; ?>/Announcements/Index">Announcements</a>
+  <a href="<?php echo URLROOT; ?>/CultivationQuestions/Index">Cultivation question</a>
+</div>
